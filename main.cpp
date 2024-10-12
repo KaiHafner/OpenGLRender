f60bd405b8df3df0957d23c7eb38bbc54bd8f67f
KaiHafner <kai.hafner@outlook.com>
2024-10-12 15:37:44 +0100

    added GLM lib

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stb/stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include"Texture.h"
 #include "shaderClass.h"
