b7d4e81095eaa267b5500a1624873fa1fd4af055
KaiHafner <kai.hafner@outlook.com>
2024-10-14 11:38:02 +0100

    Pyramid Created!

--- default.vert
+++ default.vert
@@ -11,9 +11,14 @@ out vec2 texCoord;
 
 uniform float scale;
 
+//Inputs the matrices needed for 3D Viewing perspective
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 proj;
+
 void main()
 {
-   gl_Position = vec4(aPos.x + aPos.x * scale, aPos.y + aPos.y * scale, aPos.z + aPos.z * scale, 1.0);
+   gl_Position = proj * view * model * vec4(aPos, 1.0);
    colour = aColour;
    texCoord = aTex;
 }
--- main.cpp
+++ main.cpp
@@ -14,19 +14,27 @@
 
 using namespace std;
 
+const unsigned int width = 800;
+const unsigned int height = 800;
+
 //Setting vertices coordinates
 GLfloat vertices[] =
 { //     COORDINATES     /        COLORS      /   TexCoord  //
-    -0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,	0.0f, 0.0f, // Lower left corner
-    -0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f,	0.0f, 1.0f, // Upper left corner
-     0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f,	1.0f, 1.0f, // Upper right corner
-     0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f,	1.0f, 0.0f  // Lower right corner
+    -0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+    -0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+     0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+     0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+     0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	2.5f, 5.0f
 };
 
 GLuint indices[] =
 {
-    0, 2, 1, // Upper triangle
-    0, 3, 2 // Lower triangle
+    0, 1, 2,
+    0, 2, 3,
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4,
+    3, 0, 4
 };
 
 int main(void)
@@ -41,7 +49,7 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     //Creates a GLFWwindow object
-    GLFWwindow* window = glfwCreateWindow(800, 800, "Renderer", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(width, height, "Renderer", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -52,7 +60,7 @@ int main(void)
     glfwMakeContextCurrent(window);
 
     gladLoadGL(); //Loads GLAD so its configured to OpenGL
-    glViewport(0, 0, 800, 800); //Specifies viewport of OpenGL in the window
+    glViewport(0, 0, width, height); //Specifies viewport of OpenGL in the window
 
     // Generates Shader object using shaders defualt.vert and default.frag
     Shader shaderProgram("default.vert", "default.frag");
@@ -78,20 +86,57 @@ int main(void)
 
     GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
+    //Textures
     Texture dirt("dirt.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGB, GL_UNSIGNED_BYTE);
     dirt.texUnit(shaderProgram, "tex0", 0);
+
+    Texture brick("brick.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGB, GL_UNSIGNED_BYTE);
+    brick.texUnit(shaderProgram, "tex1", 0);
+
+    //Variables for pyramid rotation
+    float rotation = 0.0f;
+    double prevTime = glfwGetTime();
+
+    //Enables depth buffer
+    glEnable(GL_DEPTH_TEST);
 
     while (!glfwWindowShouldClose(window)) //Loops until window closed
     {
         glClearColor(0.07f, 0.13f, 0.17f, 1.0f); // Specify the color of the background
-        glClear(GL_COLOR_BUFFER_BIT); // Clean the back buffer and assign the new color to it
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clean the back buffer and depth buffer
         shaderProgram.Activate(); // Tell OpenGL which Shader Program we want to use
 
+        //Simple timer
+        double currentTime = glfwGetTime();
+        if (currentTime - prevTime >= 1 / 60) 
+        {
+            rotation += 0.5f;
+            prevTime = currentTime;
+        }
+
+        //Initialise matrices so they are not the null matrix
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view = glm::mat4(1.0f); 
+        glm::mat4 proj = glm::mat4(1.0f); 
+
+        //Assigns different transformations to each matrix
+        model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+        view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+        proj = glm::perspective(glm::radians(45.0f), (float)(width / height), 0.1f, 100.0f);
+
+        //Outputs the matrices into the vertex shader
+        int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
         glUniform1f(uniID, 0.5f);
-        dirt.Bind();
+        brick.Bind(); //binds texture
 
         VAO1.Bind(); // Bind the VAO so OpenGL knows to use it
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // Draw primitives, number of indices, datatype of indices, index of indices
+        glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0); // Draw primitives, number of indices, datatype of indices, index of indices
         glfwSwapBuffers(window); // Swap the back buffer with the front buffer
         glfwPollEvents(); // Take care of all GLFW events
     }
